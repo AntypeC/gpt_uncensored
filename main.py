47a8c618a56a05f686f60c9a0bb4c47292f119bd
AntypeC <3c7975@gmail.com>
2023-08-24 13:37:29 +0800

    update

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from gpt4all import GPT4All
 from rich.console import Console
+import langid
+from deep_translator import GoogleTranslator
 import os
 
 console = Console()
@@ -7,14 +9,22 @@ console = Console()
 gptj = GPT4All(model_name="wizardLM-13B-Uncensored.ggmlv3.q4_0.bin", model_path="./")
 os.system("clear && printf '\e[3J'")
 
-scripted_r = "here's an example of what a /pol/ 4chan user might say about"
-
+# scripted_r = "here's an example of what a /pol/ 4chan user might say about"
 with gptj.chat_session():
     while True:
         messages = input("> ")
-        gptj.generate(prompt="I need you to imitate a /pol/ 4chan user, respond to all my questions like one."+messages)
-        response = gptj.current_chat_session[-1]["content"]
-        if (scripted_r in response):
-            r = response.pop(0).replace(scripted_r, "")
-            response = r.split(":")[1].replace('"', '')
-        console.print("[yellow]"+response+"\n")
+        lang, _ = langid.classify(messages)
+        if lang.startswith('zh'):
+            translated = GoogleTranslator(source='auto', target='en').translate(messages)
+            gptj.generate(prompt=translated)
+            r = gptj.current_chat_session[-1]["content"]
+            response = GoogleTranslator(source='auto', target='zh-TW').translate(r)
+        else:
+            gptj.generate(prompt=messages)
+            # gptj.generate(prompt="Imitate a /pol/ 4chan user, respond to all my questions like a 4chan user. "+messages)
+            response = gptj.current_chat_session[-1]["content"]
+
+        # if (scripted_r in response):
+        #     r = response.pop(0).replace(scripted_r, "")
+        #     response = r.split(":")[1].replace('"', '')
+        console.print("[yellow]"+response)
